d7e8fe42f8442881d3a73c8ecc177dd6a3755014
bruno coelho <bruno.coelho@gmail.com>
2014-05-27 23:02:04 +0100

    defines notes model

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var App = Ember.Application.create();
+App.Note = DS.Model.extend({
+  title: DS.attr( 'string' ),
+  body: DS.attr( 'string' )
+});
 
 App.Router.map(function() {
   this.resource("notes");
