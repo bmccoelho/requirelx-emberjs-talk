304cd88f7f309fd248aa7e9e9a98837a5e1722a1
bruno coelho <bruno.coelho@gmail.com>
2014-05-27 23:02:49 +0100

    sets application adapter to fixtures adapter and defines some fixtures

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var App = Ember.Application.create();
+App.ApplicationAdapter = DS.FixtureAdapter;
+
 App.Note = DS.Model.extend({
   title: DS.attr( 'string' ),
   body: DS.attr( 'string' )
 });
+
+App.Note.FIXTURES = [
+  { id: 1, title: 'joke', body: 'ah! ah!' },
+  { id: 2, title: 'idea', body: 'do NOT do talks about ember.js' }
+];
 
 App.Router.map(function() {
   this.resource("notes");
